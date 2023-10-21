3f5aeee6f0f099c6410cc76a3c8a69e30abc027b
Jiechen Li <jl1254@duke.edu>
2023-10-20 21:06:33 -0400

    Integrated rust_project into the main repository

---
+++ rust_project/src/main.rs
@@ -0,0 +1,52 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+use csv::Reader;
+use anyhow::Result;
+use std::collections::HashMap;
+
+pub fn read_resume(file_path: &str) -> Result<HashMap<String, Vec<String>>> {
+    let mut resume_data: HashMap<String, Vec<String>> = HashMap::new();
+    let file = std::fs::File::open(file_path)?;
+    let mut reader = Reader::from_reader(file);
+    let headers = reader.headers()?.clone();
+    for header in &headers {
+        resume_data.insert(header.to_string(), Vec::new());
+    }
+    for record in reader.records() {
+        let record = record?;
+        for (i, value) in record.iter().enumerate() {
+            let header = headers[i].to_string();
+            resume_data.get_mut(&header).unwrap().push(value.to_string());
+        }
+    }
+    Ok(resume_data)
+}
+
+pub fn check_variable(resume: &HashMap<String, Vec<String>>) {
+    let variable_list = [
+        "employment_holes",
+        "volunteer",
+        "worked_during_school",
+        "special_skills",
+    ];
+    for variable in variable_list.iter() {
+        if let Some(values) = resume.get(*variable) {
+            let mut value_counts: HashMap<String, u32> = HashMap::new();
+            for value in values {
+                *value_counts.entry(value.to_string()).or_insert(0) += 1;
+            }
+            println!("{:?}", value_counts);
+            println!("=======================");
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let file_path = "/Users/castnut/Desktop/706_Data_Engineering/Mini_Project/resume.csv";
+    let resume = read_resume(file_path)?;
+    check_variable(&resume);
+    Ok(())
+}
+
